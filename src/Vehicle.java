fb87ef79ad87542c7ef42f396e6b09a512963a90
matsb-2 <zsapekova@gmail.com>
2025-12-29 19:43:54 +0500

    Add abstract Vehicle class

---
+++ src/Vehicle.java
@@ -0,0 +1,27 @@
+public abstract class Vehicle {
+
+    protected String brand;
+    protected int year;
+    protected Driver driver; // композиция
+
+    public Vehicle(String brand, int year, Driver driver) {
+        this.brand = brand;
+        this.year = year;
+        this.driver = driver;
+    }
+
+    public abstract void startEngine();
+    public abstract void stopEngine();
+
+    public void displayInfo() {
+        System.out.println("Brand: " + brand + ", Year: " + year);
+    }
+
+    public void displayDriver() {
+        if (driver != null) {
+            driver.displayDriverInfo();
+        } else {
+            System.out.println("No driver assigned");
+        }
+    }
+}
